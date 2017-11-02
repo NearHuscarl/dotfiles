84736fbefcb5f6d07c86879cd857ae0d1dcabd5c
Near Huscarl <near.huscarl@gmail.com>
2017-11-03 03:26:45 +0700

    add script to auto update i3.win

---
+++ script/sync_i3_win.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+"""
+This script sync the content of i3/config.win from i3/config.ctrl
+config.ctrl is i3 config file with ctrl is the modifier key
+config.win  is i3 config file with win  is the modifier key
+"""
+
+import os
+from os import path
+
+home_dir = os.environ['HOME']
+i3_ctrl  = path.join(home_dir, '.config/i3/config.ctrl')
+i3_win   = path.join(home_dir, '.config/i3/config.win')
+
+if path.isfile(i3_ctrl):
+    with open(i3_ctrl, 'r') as file:
+        filedata = file.read()
+
+    filedata = filedata.replace( 'set $mod        Control', 'set $mod        Win')
+
+    with open(i3_win, 'w') as file:
+        file.write(filedata)
+
+# vim: nofoldenable
