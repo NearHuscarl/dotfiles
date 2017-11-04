c3bedd14e5e54128ea61a816c296d51d253823b2
Near Huscarl <near.huscarl@gmail.com>
2017-11-05 02:15:51 +0700

    fix variable redefined, convert spaces into tabs

--- .npm-init.js
+++ .npm-init.js
@@ -1,39 +1,38 @@
-let fs = require('fs'),
-   path = require('path'),
-   homeDir = process.env.HOME,
-   cwd = process.cwd(),
-   name = cwd.split('/').slice(-1).pop();
+let fs = require('fs');
+let path = require('path');
+let homeDir = process.env.HOME;
+let cwd = process.cwd();
+let parentDir = cwd.split('/').slice(-1).pop();
 
 // name property in package.json only accept lowercase chars and dash
-function transformName(name)
-{
-   return name.toLowerCase().replace(/[^a-z ]/g, '').replace(/\s+/g, '-');
+function transformName(name) {
+	return name.toLowerCase().replace(/[^a-z ]/g, '').replace(/\s+/g, '-');
 }
 
 function getGlobalGitConfig() {
-   let configPath = [ path.join(homeDir,  '.gitconfig'), path.join(homeDir, '.config/git/config') ];
+	let configPath = [ path.join(homeDir,  '.gitconfig'), path.join(homeDir, '.config/git/config') ];
 
-   for (let i = 0; i < configPath.length; i++) {
-      if (fs.existsSync(configPath[i])) {
-         let config = fs.readFileSync(configPath[i], 'utf-8'),
-            name = config.match(/\[user\][^[]+name\s+=\s+([^\n]+)\n/)[1],
-            email = config.match(/\[user\][^[]+email\s+=\s+([^\n]+)\n/)[1];
+	for (let i = 0; i < configPath.length; i++) {
+		if (fs.existsSync(configPath[i])) {
+			let config = fs.readFileSync(configPath[i], 'utf-8'),
+				name = config.match(/\[user\][^[]+name\s+=\s+([^\n]+)\n/)[1],
+				email = config.match(/\[user\][^[]+email\s+=\s+([^\n]+)\n/)[1];
 
-         return {name: name, email: email};
-      }
-   }
-   console.log('There is no global git config in $HOME');
+			return {name: name, email: email};
+		}
+	}
+	console.log('There is no global git config in $HOME');
 }
 
 let git = getGlobalGitConfig();
-let name = transformName(name);
+parentDir = transformName(parentDir);
 
 const packageConfig = {
-   name: name,
-   description: '',
-   version: '1.0.0',
-   author: `${git.name} <${git.email}>`,
-   license: "BSD-3-Clause"
+	name: parentDir,
+	description: '',
+	version: '1.0.0',
+	author: `${git.name} <${git.email}>`,
+	license: "BSD-3-Clause"
 };
 
 module.exports = packageConfig;
