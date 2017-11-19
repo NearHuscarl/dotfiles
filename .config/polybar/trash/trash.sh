990f8e7669de4ef23d744845ed0fb8d5e137e175
Near Huscarl <near.huscarl@gmail.com>
2017-11-20 03:14:23 +0700

    polybar trash module: add trash size

--- .config/polybar/trash/trash.sh
+++ .config/polybar/trash/trash.sh
@@ -7,7 +7,8 @@ paren_dir="$(dirname "$(readlink -f "$0")")"
 case ${1:-print} in
 	'print')
 		trash_count=$(ls -A "$trash_dir"/files/ -1 -U 2> /dev/null | wc -l)
-		echo $trash_count
+		trash_size=$(du --human-readable --apparent-size $trash_dir/files/ | awk '{print $1}')
+		echo "$trash_count ($trash_size)"
 		;;
 	'open')
 		$TERMINAL --title=Floating --default-working-directory="$trash_dir/files/" -e ranger
