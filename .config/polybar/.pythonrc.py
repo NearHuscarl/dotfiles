cd7a3ef9f69c22fbc39e0f6f48774e97347312b7
Near Huscarl <near.huscarl@gmail.com>
2017-12-05 14:42:49 +0700

    add variables and debug function to play with in interactive mode

--- .config/polybar/.pythonrc.py
+++ .config/polybar/.pythonrc.py
@@ -1,11 +1,23 @@
 #!/bin/env python
 
+# pylint: disable=unused-import
 import sys
 import os
 
 pyrc_path = os.path.join(os.environ['HOME'], '.pythonrc.py')
-os.system('python ' + pyrc_path)
+
+# pylint: disable=exec-used
+with open(pyrc_path) as file:
+	code = compile(file.read(), "code_object.py", 'exec')
+	exec(code)
 
 # Do stuff here for polybar_module environemnt only
+# pylint: disable=wrong-import-position
+import logging
+import feedparser
+
+import requests
+from requests.exceptions import HTTPError, Timeout
+from bs4 import BeautifulSoup as soup
 
 # vim: nofoldenable
--- .pythonrc.py
+++ .pythonrc.py
@@ -6,15 +6,12 @@ to setup stuff. Remember to put export PYTHONSTARTUP=$HOME/.pythonrc.py
 in .bashrc
 """
 
+# pylint: disable=unused-import
+
 # Import common modules
 import os
 import sys
-import pprint
-
-try:
-	import requests
-except ImportError:
-	print('Some modules is missing')
+from pprint import pprint as p
 
 # Fix interactive prompt with virtualenv dont have autocomplete
 import rlcompleter
@@ -31,17 +28,54 @@ except IOError:
 
 atexit.register(readline.write_history_file, histfile)
 
+# pylint: disable=too-few-public-methods
 class Exit(object):
-	"""
-	hack to alias e to sys.exit() because
-	CTRL-D is mapped to something else in i3wm
-	"""
+	""" hack to alias e to sys.exit() because CTRL-D is mapped to something else in i3wm """
 	def __repr__(self):
 		sys.exit()
 
 # Caveat: do not assign e to something else
 # Do not use e like in print(e) or it will exit the shell
 e = Exit()
-exit = Exit()
+
+# Import common module
+# pylint: disable=wrong-import-position
+from datetime import datetime
+import time
+
+def get_var_name(obj, namespace):
+	""" return variable name. Only work with one variable assignment """
+	return ''.join([name for name in namespace if namespace[name] is obj])
+
+def debug(varname, delimiter='\n'):
+	""" Print variable name and its value """
+	print(str(get_var_name(varname, globals())) + ': ' + str(varname), end=delimiter)
+
+dict1 = {'one': 'mot', 'two': 'hai', 'three': 'ba', 'four': 'bon'}
+dict2 = {'dog': 'cat', 'rich': 'poor', 'me': 'you'}
+
+list1 = ['this', 'is', 'an', 'example', 'list']
+list2 = ['another', 'example', 'list', 'to', 'test']
+
+str1 = 'I hate monday'
+str2 = 'some random string val'
+
+int1 = 1
+int2 = -9
+
+float1 = 5.00
+float2 = -6.9
+
+print()
+debug(dict1)
+debug(dict2, delimiter='\n\n')
+debug(list1)
+debug(list2, delimiter='\n\n')
+debug(str1)
+debug(str2, delimiter='\n\n')
+debug(int1)
+debug(int2, delimiter='\n\n')
+debug(float1)
+debug(float2, delimiter='\n\n')
 
 # vim: nofoldenable
