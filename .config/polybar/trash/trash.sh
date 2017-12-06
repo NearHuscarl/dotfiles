262073d49a0191d5b8a9985239ef5eebc508b883
Near Huscarl <near.huscarl@gmail.com>
2017-12-07 04:19:46 +0700

    fix trash size not show properly, update filename

--- .config/polybar/trash/trash.sh
+++ .config/polybar/trash/trash.sh
@@ -7,8 +7,9 @@ paren_dir="$(dirname "$(readlink -f "$0")")"
 case ${1:-print} in
 	'print')
 		trash_count=$(ls -A "$trash_dir"/files/ -1 -U 2> /dev/null | wc -l)
-		trash_size=$(du --human-readable --apparent-size $trash_dir/files/ | awk '{print $1}')
-		echo "$trash_count ($trash_size)"
+		trash_size=$(du --total --human-readable --apparent-size $trash_dir/files/ \
+			| tail -n 1 | awk '{print $1}')
+		echo ""$trash_count" ("$trash_size")"
 		;;
 	'open')
 		$TERMINAL --title=Floating --default-working-directory="$trash_dir/files/" -e ranger
