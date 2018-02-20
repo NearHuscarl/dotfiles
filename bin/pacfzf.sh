a4736bd9ccf75072c1bbb0861dfb213e0017e4a8
Near Huscarl <near.huscarl@gmail.com>
2018-02-20 17:00:20 +0700

    argument handling for pacfzf

--- bin/pacfzf.sh
+++ bin/pacfzf.sh
@@ -5,13 +5,27 @@
 
 # Dependencies:
 # pacman
+# trizen
 # fzf
+
+
+# BLACK="$(tput setaf 0)"
+RED="$(tput setaf 1)"
+GREEN="$(tput setaf 2)"
+# YELLOW="$(tput setaf 3)"
+# BLUE="$(tput setaf 4)"
+# MAGENTA="$(tput setaf 5)"
+# CYAN="$(tput setaf 6)"
+# WHITE="$(tput setaf 7)"
+RESET="$(tput sgr0)"
 
 SCRIPT_NAME=$(basename "$0")
 USAGE="Usage: $SCRIPT_NAME [help]"
 HELP="\
-$SCRIPT_NAME <command>
-A wrapper script to integrate fzf with pacman
+Description:
+	A wrapper script to integrate fzf with pacman
+Usage:
+	$SCRIPT_NAME <command>
 Commands:
   -h, --help          print this help message
   -I, --info          print package info
@@ -22,6 +36,7 @@ Commands:
 AUR_PKG_URL=https://aur.archlinux.org/packages.gz
 CACHE_PATH="$HOME/.cache/pacfzf"
 BACKUP_OFFICIAL="$CACHE_PATH"/Official_E
+BACKUP_OFFICIAL_DEPS="$CACHE_PATH"/Official_D
 BACKUP_AUR="$CACHE_PATH"/AUR_E
 OFFICIAL_PKGS="$CACHE_PATH/Official"
 AUR_PKGS="$CACHE_PATH/AUR"
@@ -59,64 +74,67 @@ function update_cache() { # {{{
 }
 # }}}
 function search() { # {{{
-	local packages="$1"
-	local select
-	select="$(echo "$packages" | fzf --prompt='package: ' --height=30)"
-	[[ "$select" == '' ]] && exit 55 || echo "$select"
+	fzf --prompt='package: ' --height=30
 }
 # }}}
 function get_all_local_packages() { # {{{
-	local pkg
-	pkg="$(pacman -Qq)"
-	search "$pkg"
+	pacman -Qq
 }
 # }}}
 function get_all_remote_packages() { # {{{
-	local pkg
-	pkg="$(cat "$OFFICIAL_PKGS" "$AUR_PKGS")"
-	search "$pkg"
+	cat "$OFFICIAL_PKGS" "$AUR_PKGS"
 }
 # }}}
 function get_official_local_packages() { # {{{
-	local pkg
-	pkg="$(pacman -Qq | grep -v -Qmq)"
-	search "$pkg"
+	pacman -Qnq
 }
 # }}}
 function get_official_remote_packages() { # {{{
-	local pkg
-	pkg="$(cat "$OFFICIAL_PKGS")"
-	search "$pkg"
+	cat "$OFFICIAL_PKGS"
 }
 # }}}
 function get_aur_local_packages() { # {{{
-	local pkg
-	pkg="$(pacman -Qmq)"
-	search "$pkg"
+	pacman -Qmq
 }
 # }}}
 function get_aur_remote_packages() { # {{{
-	local pkg
-	pkg="$(cat "$AUR_PKGS")"
-	search "$pkg"
+	cat "$AUR_PKGS"
+}
+# }}}
+function get_packages() { # {{{
+	local repo="$1"
+	if "$repo" == 'official'; then
+		get_official_remote_packages
+	elif "$repo" == 'aur'; then
+		get_aur_remote_packages
+	elif "$repo" == 'all'; then
+		get_all_remote_packages
+	fi
 }
 # }}}
 function info() { # {{{
 	local package
-	package="$(get_all_remote_packages)"
-	pacman -Si "$package"
+	package="$(get_all_remote_packages | search)"
+	[[ "$package" == '' ]] \
+		&& trizen -Si "$package" 2> /dev/null \
+		|| pacman -Si "$package" 2> /dev/null
 }
 # }}}
 function backup_official() { # {{{
-	pacman -Qeq | grep -v "$(pacman -Qmq)" > "$BACKUP_OFFICIAL"
+	pacman -Qneq > "$BACKUP_OFFICIAL"
 }
 # }}}
 function backup_aur() { # {{{
 	pacman -Qmeq > "$BACKUP_AUR"
 }
 # }}}
+function backup_offical_deps() { # {{{
+	comm -13 <(pacman -Qqdt | sort) <(pacman -Qqdtt | sort) > "$BACKUP_OFFICIAL_DEPS"
+}
+# }}}
 function install_official_backup() { # {{{
 	xargs pacman -S --needed --noconfirm < "$BACKUP_OFFICIAL"
+	xargs pacman -S --needed --noconfirm --asdeps < "$BACKUP_OFFICIAL_DEPS"
 }
 # }}}
 function install() { # {{{
@@ -126,8 +144,8 @@ function install() { # {{{
 # }}}
 function remove() { # {{{
 	local package
-	package="$(get_local_packages)"
-	sudo pacman -Rs "$package"
+	package="$(get_all_local_packages)"
+	[[ "$package" == '' ]] && exit 55 || sudo pacman -Rs "$package"
 }
 # }}}
 function update() { # {{{
@@ -143,27 +161,93 @@ function dependencies() { # {{{
 }
 # }}}
 function main() { # {{{
-	local cmd="${1:-}"
+	# help info install remove dependencies
+	local OPTIND opt cmd repo
 
-	case "$#" in
-		0)
-			usage ;;
-		*)
-			case "$cmd" in
-				-h|--help)
-					help ;;
-				-I|--info)
-					info ;;
-				-i|--install)
-					install ;;
-				-r|--remove)
-					remove ;;
-				-d|--dependencies)
-					dependencies ;;
-				*)
-					usage ;;
-			esac
-	esac
+	# shellcheck disable=SC2059
+	function handle_operation_opt() { # {{{
+		if [[ "${cmd:-}" == '' ]]; then
+			cmd="$1"
+		else
+			printf "${RED}"'Error:'"${RESET}"' Only one operation is allowed\n'
+			exit 1
+		fi
+	}
+	# }}}
+	# shellcheck disable=SC2059
+	function handle_repo_opt() { # {{{
+		if [[ "${repo:-}" == '' ]]; then
+			repo="$1"
+		else
+			if [[ "$repo" == "$1" ]] || [[ "$repo" == all ]]; then
+				printf "${RED}"'Error:'"${RESET}"' Specify to search only in official or AUR repo\n'
+				exit 1
+			else
+				repo=all
+			fi
+		fi
+	}
+	# }}}
+
+	while getopts ':hiIrao-:' opt; do
+		case "${opt}" in
+			-)
+				case ${OPTARG} in # a hack to use --option
+					help)
+						handle_operation_opt help
+						;;
+					info)
+						handle_operation_opt info
+						;;
+					install)
+						handle_operation_opt install
+						;;
+					remove)
+						handle_operation_opt remove
+						;;
+					aur)
+						handle_repo_opt aur
+						;;
+					official)
+						handle_repo_opt official
+						;;
+					*)
+						printf "${RED}Error:${RESET} Invalid argument ${GREEN}--%s"'\n'"${RESET}" "$OPTARG"
+						exit 1
+						;;
+				esac
+				;;
+			h)
+				handle_operation_opt help
+				;;
+			I)
+				handle_operation_opt info
+				;;
+			i)
+				handle_operation_opt install
+				;;
+			r)
+				handle_operation_opt remove
+				;;
+			a)
+				handle_repo_opt aur
+				;;
+			o)
+				handle_repo_opt official
+				;;
+			*)
+				printf "${RED}Error:${RESET} Invalid argument ${GREEN}-%s"'\n'"${RESET}" "$OPTARG"
+				exit 1
+				;;
+		esac
+	done
+	: "${repo:=official}"
+	[[ ! "${cmd:-}" ]] && printf "${RED}"'Error: '"${RESET}"'Missing operation\n'"${RESET}" && exit 1
+	printf 'cmd: %s\n' "$cmd"
+	printf 'repo: %s\n' "$repo"
+	shift $((OPTIND-1))
+
+	eval "$cmd"
 }
 
 main "$@"
