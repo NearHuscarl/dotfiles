4e10078bd550a3229a8b036955386c02c725359f
Near Huscarl <near.huscarl@gmail.com>
2018-02-24 22:26:25 +0700

    whitespace

--- script/python/sync_i3_win.py
+++ script/python/sync_i3_win.py
@@ -16,9 +16,9 @@ i3_win = path.join(home_dir, '.config/i3/config.win')
 if path.isfile(i3_ctrl):
 	with open(i3_ctrl, 'r') as file:
 		filedata = file.read()
-		
+
 	filedata = filedata.replace('Control', 'Mod4')
-		
+
 	with open(i3_win, 'w') as file:
 		file.write(filedata)
 
