efe879bb8c463d727496ff802674e835feb3639c
NearHuscarl <near.huscarl@gmail.com>
2018-08-24 21:59:54 +0700

    refactor

--- script/bash/lib.sh
+++ script/bash/lib.sh
@@ -6,3 +6,31 @@ function gitroot() { # {{{
 	git rev-parse --show-toplevel 2> /dev/null
 }
 # }}}
+function has_command() { # {{{
+	# check command avalability
+	# usage:
+	# $ if has_command vim; then
+	# $    vim /tmp/temp
+	# $ fi
+	command -v $1 > /dev/null
+}
+# }}}
+function notify() { # {{{
+	local title="$1" message="$2" icon="$3"
+	if [[ -x /usr/share/dunstify ]]; then
+		dunstify --replace=101 --urgency=low --icon="$icon" "$message"
+	else
+		notify-send --urgency=low --icon="$icon" "$title" "$message"
+	fi
+}
+# }}}
+get_script_dir() { # {{{
+	SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
+}
+# }}}
+read_file_content() { # {{{
+	while read -r line ; do
+		CONTENT+="$line "
+	done < "/path/to/file"
+}
+# }}}
