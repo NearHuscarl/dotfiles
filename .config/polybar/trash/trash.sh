472bdde23f56282f4acf54c9afa88071ec39893a
NearHuscarl <near.huscarl@gmail.com>
2018-09-07 01:22:35 +0700

    cleanup

--- .config/polybar/trash/trash.sh
+++ .config/polybar/trash/trash.sh
@@ -3,32 +3,41 @@
 # Dependencies:
 # alsa
 
-trash_dir="$HOME/.local/share/Trash"
-parent_dir="$(dirname "$(readlink -f "$0")")"
-icon_path="$HOME/.icons/dunst"
-display_path="$(dirname "$0")"/display
+TRASH_PATH="$HOME/.local/share/Trash"
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+ICON_PATH="$HOME/.icons/dunst"
+CACHE_PATH="$HOME/.cache/polybar/trash"
+DEFAULT_DISPLAY='size' # size or count
 
 
 function set_display_state() { # {{{
-	echo "$1" > "$display_path"
+	if [[ ! -d "$CACHE_PATH" ]]; then
+		mkdir -pv "$CACHE_PATH"
+	fi
+	echo "$1" > "$CACHE_PATH/display"
 }
 # }}}
 function get_display_state() { # {{{
-	head -n 1 "$display_path"
-}
-# }}}
-function print_trash_count() { # {{{
-	# shellcheck disable=SC2012
-	ls -A "$trash_dir/files/" -1 -U 2> /dev/null | wc -l
-	set_display_state 'count'
+	if [[ ! -d "$CACHE_PATH" ]]; then
+		mkdir -pv "$CACHE_PATH"
+		echo "$DEFAULT_DISPLAY" > "$CACHE_PATH/display"
+	fi
+	head -n 1 "$CACHE_PATH/display"
 }
 # }}}
 function get_size() { # {{{
 	du --total --human-readable --apparent-size "$1"
 }
 # }}}
+
+function print_trash_count() { # {{{
+	# shellcheck disable=SC2012
+	ls -A "$TRASH_PATH/files/" -1 -U 2> /dev/null | wc -l
+	set_display_state 'count'
+}
+# }}}
 function print_trash_size() { # {{{
-	get_size "$trash_dir"/files/ | tail -n 1 | awk '{print $1}'
+	get_size "$TRASH_PATH"/files/ | tail -n 1 | awk '{print $1}'
 	set_display_state 'size'
 }
 # }}}
@@ -36,6 +45,7 @@ function print_trash_info() { # {{{
 	# print trash info based on current display state (count or size)
 	display_state="$(get_display_state)"
 
+	echo "$(print_trash_count)/$(print_trash_size)"
 	if [[ "$display_state" == 'count' ]]; then
 		print_trash_count
 	elif [[ "$display_state" == 'size' ]]; then
@@ -43,6 +53,7 @@ function print_trash_info() { # {{{
 	fi
 }
 # }}}
+
 function toggle_format() { # {{{
 	display_state="$(get_display_state)"
 
@@ -53,23 +64,29 @@ function toggle_format() { # {{{
 	fi
 }
 # }}}
+function open_trash() { # {{{
+	xdg-open "$TRASH_PATH" &> /dev/null
+}
+# }}}
 function clean_trash() { # {{{
-	rm -rf "$trash_dir"/{files,info}/*
+	rm -rf "$TRASH_PATH"/{files,info}/*
+	dunstify --urgency=low --icon="$ICON_PATH/trash.png" 'Trash has been deleted'
+	aplay "$SCRIPT_DIR/trash-empty-sound.wav" &> /dev/null &
 	print_trash_info
-	dunstify --urgency=low --icon="$icon_path/trash.png" 'Trash has been deleted'
-	aplay "$parent_dir/trash-empty-sound.wav" &> /dev/null &
 }
 # }}}
 
 case ${1:-count} in
-	print)
-		print_trash_info ;;
-	toggle)
-		toggle_format ;;
 	count)
 		print_trash_count ;;
 	size)
 		print_trash_size ;;
+	print)
+		print_trash_info ;;
+	toggle)
+		toggle_format ;;
+	open)
+		open_trash ;;
 	clean)
 		clean_trash ;;
 	*)
