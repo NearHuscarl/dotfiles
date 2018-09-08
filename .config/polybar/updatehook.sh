8ae57fa9f9a3f81a1bd6c6e9a76de8fc82057e47
NearHuscarl <near.huscarl@gmail.com>
2018-09-09 03:07:14 +0700

    use systemd timer to update crypto module in polybar

--- .config/polybar/updatehook.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/env bash
-
-# general script to update polybar hooks after x interval
-
-echo "running $HOOK_NAME hook watcher.."
-while sleep "$UPDATE_INTERVAL"; do
-	polybar-msg hook "$HOOK_NAME" "$HOOK_INDEX"
-done
