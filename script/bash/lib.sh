c984e9d53f7a32dc98bdaf8a78788466e3b396ab
NearHuscarl <near.huscarl@gmail.com>
2018-09-15 02:42:49 +0700

    backup

--- script/bash/lib.sh
+++ script/bash/lib.sh
@@ -47,10 +47,21 @@ get_script_dir() { # {{{
 	SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 }
 # }}}
+
 read_file_content() { # {{{
 	while read -r line ; do
 		CONTENT+="$line "
 	done < "/path/to/file"
+}
+# }}}
+read_file_and_remove_comment() { # {{{
+	while read -r line ; do
+		line=${line%\#*} # remove comment
+		if [[ "$line" =~ ^\ *$ ]]; then
+			continue
+		fi
+		lines+="$line "
+	done < "$file"
 }
 # }}}
 
@@ -70,5 +81,21 @@ has_package() { # {{{
 	# $ fi
 	pacman -Q "$1" &> /dev/null && return 0;
 	return 1
+}
+# }}}
+
+is_in_virtual_environment() { # {{{
+	# grep -q ^flags.*\ hypervisor\ /proc/cpuinfo && return 0
+	# return 1
+	if [[ "$(systemd-detect-virt)" == none ]]; then
+		return 0
+	fi
+	return 1
+}
+# }}}
+update_option() { # {{{
+	# update option value, take one argument is the option name
+	local option="$1" value="${!1}" # get the variable value where variable name is stored as a string
+	sed -i "/^${option}=/s/^.*$/${option}='$value'/" "$FILE_NAME"
 }
 # }}}
